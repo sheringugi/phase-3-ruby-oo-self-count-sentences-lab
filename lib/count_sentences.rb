bfbedc09f4ba1e228de7a263696580108d63f788
sheringugi <sheri.ngugi17@gmail.com>
2023-05-17 15:00:40 +0300

    complete assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
+    if "#{self}".end_with?(".")
+      true
+    else
+      false
+    end
     
   end
 
   def question?
+    if "#{self}".end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    if "#{self}".end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
+    # if "#{self}".split(".","!","?","...").count
+    # else
+    #   !countdef count_sentences
+    "#{self}".split(/[.?!]/).reject(&:empty?).count
   end
 end
